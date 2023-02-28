5e593df6e25632603ee433a2f604b811e2a72a55
KillerD101 <k9d1y0an@gmail.com>
2023-02-28 06:05:48 -0500

    First working implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::Read;
+
 const DEFAULT_BOARD_WIDTH: usize = 50;
 const DEFAULT_BOARD_HEIGHT: usize = 20;
 
@@ -31,12 +33,103 @@ impl Board {
             println!();
         }
     }
+
+    fn set_state(&mut self, next: Vec<bool>) {
+        self.board = next;
+    }
+
+    fn get(&self, col: usize, row: usize) -> bool {
+        return self.board[col * self.width + row];
+    }
+
+    fn set(&mut self, col: usize, row: usize) {
+        let index = col * self.width + row;
+
+        self.board[index] = !self.board[index];
+    }
+}
+
+fn get_neighbors(board: &Board, col: usize, row: usize) -> Vec<bool> {
+    let mut neighbors: Vec<bool> = Vec::new();
+
+    for c in -1..2 {
+        for r in -1..2 {
+            if c != 0 || r != 0 {
+                let temp = board.board.get((
+                    ((col as i32) + c) * (board.width as i32) + ((row as i32) + r)
+                ) as usize); 
+                if temp.is_some() == true {
+                    neighbors.push(*temp.unwrap());
+                }
+            }
+        }
+    }
+
+    return neighbors;
+}
+
+fn count(list: Vec<bool>) -> usize {
+    let mut c: usize = 0;
+
+    for item in list {
+        if item { c += 1; }
+    }
+
+    return c;
+}
+
+fn print_break() {
+    println!();
+
+    for _ in 0..(DEFAULT_BOARD_WIDTH + 5) {
+        print!("-");
+    }
+
+    println!();
+    println!();
+}
+
+fn wait_for_input() {
+    let mut _unused = String::new();
+    std::io::stdin().read_line(&mut _unused);
 }
 
 fn main() {
-    let board = Board::new(
+    let mut board = Board::new(
         DEFAULT_BOARD_WIDTH, DEFAULT_BOARD_HEIGHT
     );
+
+    board.set(7, 7);
+    board.set(7, 8);
+    board.set(7, 6);
+    board.set(6, 7);
+    board.set(8, 7);
  
     board.print();
+
+    wait_for_input();
+
+    loop {
+        print_break();
+
+        let mut next_state: Vec<bool> = Vec::new();
+
+        for col in 0..board.height {
+            for row in 0..board.width {
+                let ncount = count(get_neighbors(&board, col, row));
+
+                if board.get(col, row) {
+                    next_state.push(ncount == 2 || ncount == 3);
+                } else {
+                    next_state.push(ncount == 3);
+                }
+            }
+        }
+
+        board.set_state(next_state);
+
+        board.print();
+
+        wait_for_input();
+    }
 }
