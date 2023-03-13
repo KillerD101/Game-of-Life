ebdcf6e6d63a6035d7898be0e678d3819e4e3170
Dylan Sparks <dylan_sparks@caryacademy.org>
2023-03-13 10:11:48 -0400

    Removed grid lines

--- src/main.js
+++ src/main.js
@@ -1,20 +1,38 @@
 let cnv;
 
-let scene_alpha = 255;
+const scene_alpha = 255;
 
-let BACKGROUND_COLOR = {
-    r: 50, g: 50, b: 50
+const BACKGROUND_COLOR = {
+    r: 200, g: 200, b: 200
 };
 
-function resize_canvas() {
-    cnv.position(
-        (windowWidth - width) / 2, 0
-    );
+const ROWS = 50;
+const COLS = 50;
+
+let board = new Array(ROWS * COLS);
+
+let scl_width;
+let scl_height;
+
+function get_board(i, j) {
+    return board[i * COLS + j];
+}
+
+function set_board(i, j, val) {
+    if(val != undefined) board[i * COLS + j] = val;
+    else board[i * COLS + j] = !board[i * COLS + j];
 }
 
 function setup() {
     cnv = createCanvas(512, 512);
-    resize_canvas();
+    windowResized();
+
+    for(var i = 0; i < board.length; i++)
+        board[i] = false;
+
+    set_board(2, 3);
+    set_board(2, 4);
+    set_board(2, 5);
 }
 
 function draw() {
@@ -22,8 +40,21 @@ function draw() {
         BACKGROUND_COLOR.r, BACKGROUND_COLOR.g,
         BACKGROUND_COLOR.b, scene_alpha
     );
+
+    fill(0, 0, 0, scene_alpha);
+    noStroke();
+    for(var i = 0; i < COLS; i++) for(var j = 0; j < ROWS; j++)
+        if(get_board(j, i)) rect(
+            i * scl_width, j * scl_height,
+            scl_width, scl_height
+        );
 }
 
 function windowResized() {
-    resize_canvas();
+    cnv.position(
+        (windowWidth - width) / 2, 0
+    );
+
+    scl_width = width / ROWS;
+    scl_height = height / COLS;
 }
